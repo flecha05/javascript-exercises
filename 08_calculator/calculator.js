0824926d4582a8c41d7af2f0a05632cd8b9b779b
flecha05 <flechafacundo@gmail.com>
2023-09-08 11:28:43 -0300

    Working with arrays JS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr=[arr,...args]) {
+	return arr.reduce((a,b)=>a+b,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr=[arr,...args]) {
+ /* var mlt=1;
+  arr.forEach(element => {
+    mlt*=element;
+  });
+  return mlt;*/
+  return arr.reduce((a,b)=>a*b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  var pow=a;
+	for (let i = 1; i < b; i++) {
+    pow*=a;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	if (num<0) {
+    return -1;
+  }else if( num ==0){
+    return 1;
+  }else{
+    return(num*factorial(num-1));
+  }
 
+};
+//arr,...args
 // Do not edit below this line
 module.exports = {
   add,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string=string.toLowerCase().trim().replace(/[^a-z]/g,'');
 
+    var reverse= string.split(' ').reverse().join(' ');
+    return reverse===string;
 };
 
 // Do not edit below this line
